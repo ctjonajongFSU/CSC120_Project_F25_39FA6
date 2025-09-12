a7f4c6188669160ff9b2b5a0fb643b72837dcfb6
ctjonajongFSU <ctjonajong@broncos.uncfsu.edu>
2025-09-12 11:11:57 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 game_name = "PyQuest"
 print("Welcome to", game_name)
-print("==================")
+print("=================")
 name = input("Before we begin, what is your character's name?\n")
+print("Great", name + "!",  "Let's begin the adventure!")
